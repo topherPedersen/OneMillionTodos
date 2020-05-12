40ebb65bb924f249fe08809ff2a6da1460ca6dbd
topherPedersen <chrispedatx@gmail.com>
2020-05-12 10:47:13 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <SafeAreaView>
+        <Text>Ten Thousand Todos...</Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default App;
